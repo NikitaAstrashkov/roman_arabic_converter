d1ca6b2f9d96e0797ff10f4ab9f54af3394e7bbf
NikitaAstrashkov <nicitch.astrashkov@gmail.com>
2020-06-11 23:25:25 +0300

    Refactored testing & converter.

--- src/converter/nik/NumConverter.java
+++ src/converter/nik/NumConverter.java
@@ -1,136 +1,80 @@
 package converter.nik;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NumConverter {
+    static String[] romanNumbers = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}; // Цифры в числе идут только в таком порядке
+    static int[] arabicNumbers =  {1000,  900, 500,  400, 100,  90 ,  50,  40 ,  10,   9 ,  5 ,   4 ,  1 }; // Числа, соответствующие цифрам латинской СИ
+
+    static String[] romanNumbers1 = {"CM", "CD", "XC", "XL", "IX", "IV",  "M", "D", "C", "L", "X", "V", "I"}; // Цифры в числе идут только в таком порядке
+    static int[] arabicNumbers1 =    {900,  400,  90 ,  40 ,   9 ,   4 , 1000, 500, 100,  50,  10,  5 ,  1 }; // Числа, соответствующие цифрам латинской СИ
+
+    static int arabicNum;
     /*
      * Конвертация из арабской системы исчисления в латинскую.
      * */
     public static String convert(int arabic) throws IllegalArgumentException {
+        StringBuilder romanNum = new StringBuilder();
+        arabicNum = arabic;
         if (arabic > 3999 || arabic < 1) {
             throw new IllegalArgumentException("Number is out of range");
         }
-        StringBuilder roman = new StringBuilder();
-        while (arabic >= 1000) {
-            roman.append("M");
-            arabic -= 1000;        }
-        while (arabic >= 900) {
-            roman.append("CM");
-            arabic -= 900;
+        for (int j = 0; j < romanNumbers.length; j++) {
+            romanNum.append(convertParts(arabicNumbers[j], romanNumbers[j]));
         }
-        while (arabic >= 500) {
-            roman.append("D");
-            arabic -= 500;
+        return romanNum.toString();
+    }
+
+
+    private static String convertParts(int arabicDecrease, String appendix) {
+        StringBuilder currentRomanNum = new StringBuilder();
+        while (arabicNum >= arabicDecrease) {
+            currentRomanNum.append(appendix);
+            arabicNum -= arabicDecrease;
         }
-        while (arabic >= 400) {
-            roman.append("CD");
-            arabic -= 400;
-        }
-        while (arabic >= 100) {
-            roman.append("C");
-            arabic -= 100;
-        }
-        while (arabic >= 90) {
-            roman.append("XC");
-            arabic -= 90;
-        }
-        while (arabic >= 50) {
-            roman.append("L");
-            arabic -= 50;
-        }
-        while (arabic >= 40) {
-            roman.append("XL");
-            arabic -= 40;
-        }
-        while (arabic >= 10) {
-            roman.append("X");
-            arabic -= 10;
-        }
-        while (arabic >= 9) {
-            roman.append("IX");
-            arabic -= 9;
-        }
-        while (arabic >= 5) {
-            roman.append("V");
-            arabic -= 5;
-        }
-        while (arabic >= 4) {
-            roman.append("IV");
-            arabic -= 4;
-        }
-        while (arabic >= 1) {
-            roman.append("I");
-            arabic -= 1;
-        }
-        return roman.toString();
+        return currentRomanNum.toString();
     }
 
     /*
-    * Конвертация из латинской системы исчисления в арабскую.
-    * */
+     * Конвертация из латинской системы исчисления в арабскую.
+     * */
     public static int convert(String roman) throws IllegalArgumentException {
         int i = 0;
         int arabic = 0;
-        char[] romanBff = (roman + "Q").toCharArray(); // Q - символ в конце массива, нужен чтобы не вывалились за пределы массива при проверках типа charBff[i+1]
-
-        if (romanBff.length > 13) // MMMCMLXXXVIII - 13 латинских чисел, максимальное количество латинских цифросимволов в заданном диапазоне, косвенная проверка на выход за пределы.
+        StringBuilder currentRoman = new StringBuilder(roman);
+        if (roman.length() > 13) // MMMCMLXXXVIII - 13 латинских чисел, максимальное количество латинских цифросимволов в заданном диапазоне, косвенная проверка на выход за пределы.
             throw new IllegalArgumentException("Invalid string length");
         if (roman.isEmpty()) // проверяем не пуста ли предоставленная строка
             throw new IllegalArgumentException("String is empty");
-
-        while (i < romanBff.length) {
-            switch (romanBff[i]) {
-                case 'M':
-                    arabic += 1000;
-                    break;
-                case 'D':
-                    arabic += 500;
-                    break;
-                case 'C':
-                    if (romanBff[i+1] == 'M') {
-                        arabic += 900;
-                        i++;
-                    }
-                    else if (romanBff[i+1] == 'D') {
-                        arabic += 400;
-                        i++;
-                    }
-                    else arabic += 100;
-                    break;
-                case 'L':
-                    arabic += 50;
-                    break;
-                case 'X':
-                    if (romanBff[i+1] == 'C') {
-                        arabic += 90;
-                        i++;
-                    }
-                    else if (romanBff[i+1] == 'L') {
-                        arabic += 40;
-                        i++;
-                    }
-                    else arabic += 10;
-                    break;
-                case 'V':
-                    arabic += 5;
-                    break;
-                case 'I':
-                    if (romanBff[i+1] == 'X') {
-                        arabic += 9;
-                        i++;
-                    }
-                    else if (romanBff[i+1] == 'V') {
-                        arabic += 4;
-                        i++;
-                    }
-                    else arabic += 1;
-                    break;
-                case 'Q':
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid input");
-            }
-            i++;
+        //Pattern romanNumberValidation = Pattern.compile("M*D*C*L*X*V*I*", Pattern.MULTILINE);
+        //Matcher m = romanNumberValidation.matcher(roman);
+        if(!(Pattern.matches("[MDCLXVI]*", roman)))
+        {
+            throw new IllegalArgumentException("Invalid input");
         }
-        if (arabic > 3999) throw new IllegalArgumentException("Number is out of range");
+        while (true) {
+            if (currentRoman.toString().isEmpty()) {
+                break;
+            }
+            int index = currentRoman.indexOf(romanNumbers1[i]);
+            if (index == -1) {
+                i++;
+            }
+            else {
+                arabic += arabicNumbers1[i];
+                if (i < 6)
+                    currentRoman.delete(index, index+2); // Удаляем два элемента
+                else
+                    currentRoman.deleteCharAt(index); // Удаляем первый элемент
+            }
+        }
+        if (arabic > 3999)
+            throw new IllegalArgumentException("Number is out of range");
         return arabic;
+    }
+
+    private static boolean dividesByTwo(int a){
+        return (a%2==0);
     }
 }
--- src/converter/nik/NumConverterTest.java
+++ src/converter/nik/NumConverterTest.java
@@ -1,103 +1,59 @@
 package converter.nik;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class NumConverterTest {
-    String romanInvalidException1       = "";                // негативный кейс, пустая строка, вернёт ошибку "String is empty"
-    String romanInvalidException2       = "MMMDDDCCCLLLXXX"; // негативный кейс, 15 символов выходят за косвенную проверку, вернёт ошибку "Invalid string length"
-    String romanInvalidException3       = "ABEFGH";          // негативный кейс, таких символов нет в латинском исчислении, вернёт ошибку "Invalid input"
-    String romanOutOfRangeException     = "MMMM";            // негативный кейс, выход за максимальный предел, вернёт ошибку "Number is out of range"
-    String romanPassedUpperLimit        = "MMMCMXCIX";       // позитивный кейс, полученное число будет максимальным пределом
-    String romanPassedLowerLimit        = "I";               // позитивный кейс, полученное число будет минимальным пределом
-    String romanPassedMiddleOfRange     = "MCMXCIX";         // позитивный кейс, полученное число будет серединой диапазона
-
-    int    arabicOutOfRange1            = 0;                 // негативный кейс, выход за мин. предел, вернёт ошибку "Number is out of range"
-    int    arabicOutOfRange2            = 4000;              // негативный кейс, выход за макс. предел, вернёт ошибку "Number is out of range"
-    int    arabicPassedUpperLimit       = 3999;              // позитивный кейс, полученное число будет максимальным пределом
-    int    arabicPassedLowerLimit       = 1;                 // позитивный кейс, полученное число будет минимальным пределом
-    int    arabicPassedMiddleOfRange    = 1999;              //    позитивный кейс, полученное число будет серединой диапазона
-
-    /*
-     * Тест позитивных кейсов конвертаций
-     * Проверены по три значения: верхняя граница, нижняя граница, середина диапазона
-     * Тесты должны быть пройдены успешно
-     */
-    @Test
-    void positiveTestsConvert() {
-        assertEquals(romanPassedUpperLimit, NumConverter.convert(arabicPassedUpperLimit));
-        assertEquals(romanPassedLowerLimit, NumConverter.convert(arabicPassedLowerLimit));
-        assertEquals(romanPassedMiddleOfRange, NumConverter.convert(arabicPassedMiddleOfRange));
-
-        assertEquals(arabicPassedUpperLimit, NumConverter.convert(romanPassedUpperLimit));
-        assertEquals(arabicPassedLowerLimit, NumConverter.convert(romanPassedLowerLimit));
-        assertEquals(arabicPassedMiddleOfRange, NumConverter.convert(romanPassedMiddleOfRange));
+    static Stream<Arguments> positiveTestsStringsAndNumbersProvider() {
+        return Stream.of(
+                arguments("MMMCMXCIX", 3999),
+                arguments("I", 1),
+                arguments("MCMXCIX", 1999)
+        );
     }
 
-    /*
-     * Тест перевода из латинской в арабскую СИ
-     * Не пройдёт проверку т.к. на вводе пустая строка
-     * Вернёт ошибку "String is empty"
-     */
-    @Test
-    void romanToArabicNegativeTestConvert1() { //
-        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(romanInvalidException1));
-        assertEquals("String is empty", thrown.getMessage());
+    @ParameterizedTest
+    @MethodSource("positiveTestsStringsAndNumbersProvider")
+    void converter_LegalInput_true(String romanNumber, int arabicNumber) {
+        assertEquals(romanNumber, NumConverter.convert(arabicNumber)); // Проверка перевода из арабского СИ в латинский
+        assertEquals(arabicNumber, NumConverter.convert(romanNumber)); // Проверка перевода из латинского СИ в арабский
     }
 
-    /*
-     * Тест перевода из латинской в арабскую СИ
-     * Не пройдёт проверку т.к. на вводе слишком длинная строка (больше 13 символов)
-     * Вернёт ошибку "Invalid string length"
-     */
-    @Test
-    void romanToArabicNegativeTestConvert2() { //
-        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(romanInvalidException2));
-        assertEquals("Invalid string length", thrown.getMessage());
+    static Stream<Arguments> negativeRomanTestsStringsProvider() {
+        return Stream.of(
+                arguments("", "String is empty"),
+                arguments("MMMDDDCCCLLLXXX", "Invalid string length"),
+                arguments("ABEFGH", "Invalid input"),
+                arguments("MMMM", "Number is out of range")
+        );
     }
 
-    /*
-     * Тест перевода из латинской в арабскую СИ
-     * Не пройдёт проверку т.к. таких символов нет в латинском исчислении (больше 13 символов)
-     * Вернёт ошибку "Invalid input"
-     */
-    @Test
-    void romanToArabicNegativeTestConvert3() { //
-        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(romanInvalidException3));
-        assertEquals("Invalid input", thrown.getMessage());
+    @ParameterizedTest
+    @MethodSource("negativeRomanTestsStringsProvider")
+    void converter_InvalidRomanInput_ExceptionThrown(String romanInvalidNumber, String expectedBehavior) {
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(romanInvalidNumber));
+        assertEquals(expectedBehavior, thrown.getMessage());
     }
 
-    /*
-     * Тест перевода из латинской в арабскую СИ
-     * Не пройдёт проверку т.к. символьное представление выходит за верхний предел
-     * Вернёт ошибку "Number is out of range"
-     */
-    @Test
-    void romanToArabicNegativeTestConvert4() { //
-        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(romanOutOfRangeException));
-        assertEquals("Number is out of range", thrown.getMessage());
+    static Stream<Arguments> negativeArabicTestsStringsProvider() {
+        return Stream.of(
+                arguments(0, "Number is out of range"),
+                arguments(4000, "Number is out of range")
+        );
     }
 
-    /*
-     * Тест перевода из арабской в латинскую СИ
-     * Не пройдёт проверку т.к. число выходит за нижний предел
-     * Вернёт ошибку "Number is out of range"
-     */
-    @Test
-    void arabicToRomanNegativeTestConvert1() { //
-        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(arabicOutOfRange1));
-        assertEquals("Number is out of range", thrown.getMessage());
-    }
-
-    /*
-     * Тест перевода из арабской в латинскую СИ
-     * Не пройдёт проверку т.к. число выходит за верхний предел
-     * Вернёт ошибку "Number is out of range"
-     */
-    @Test
-    void arabicToRomanNegativeTestConvert2() { //
-        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(arabicOutOfRange2));
-        assertEquals("Number is out of range", thrown.getMessage());
+    @ParameterizedTest
+    @MethodSource("negativeArabicTestsStringsProvider")
+    void converter_InvalidArabicInput_ExceptionThrown(int arabicInvalidNumber, String expectedBehavior) {
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> NumConverter.convert(arabicInvalidNumber));
+        assertEquals(expectedBehavior, thrown.getMessage());
     }
 }
+
